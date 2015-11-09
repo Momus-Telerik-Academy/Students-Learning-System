58450d020a094bb7570439e6d3b49498cb53261e
MariyaSteffanova <mariya.steffanova@gmail.com>
2015-11-09 16:12:15 +0200

    [Services] Added service for managing the data in Category table from the database

---
+++ Source/Services/StudentsLearning.Services.Data/CategoryService.cs
@@ -0,0 +1,48 @@
+﻿namespace StudentsLearning.Services.Data
+{
+    using System.Linq;
+    using Contracts;
+    using StudentsLearning.Data.Models;
+    using StudentsLearning.Data.Repositories;
+
+    public class CategoryService : ICategoryService
+    {
+        private readonly IRepository<Category> categories;
+
+        public CategoryService(
+            IRepository<Category> categoriesRepository)
+        {
+            this.categories = categoriesRepository;
+        }
+
+        public void Add(string name)
+        {
+            this.categories.Add(new Category() { Name = name });
+            this.categories.SaveChanges();
+        }
+
+        public IQueryable<Category> All()
+        {
+            return this.categories.All();
+        }
+
+        public int? GetId(string name)
+        {
+            return this.categories.All()
+                .Where(x => x.Name == name)
+                .Select(c => c.Id)
+                .FirstOrDefault();
+        }
+
+        public Category GetById(int id)
+        {
+            return this.categories.GetById(id);
+        }
+
+        public void Update(Category category)
+        {
+            this.categories.Update(category);
+            this.categories.SaveChanges();
+        }
+    }
+}
---
+++ Source/Services/StudentsLearning.Services.Data/Contracts/ICategoryService.cs
@@ -0,0 +1,19 @@
+﻿namespace StudentsLearning.Services.Data.Contracts
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using StudentsLearning.Data.Models;
+
+    public interface ICategoryService
+    {
+        IQueryable<Category> All();
+
+        Category GetById(int id);
+
+        int? GetId(string name);
+
+        void Add(string category);
+
+        void Update(Category category);
+    }
+}
