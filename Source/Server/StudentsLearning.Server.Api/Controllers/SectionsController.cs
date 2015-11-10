1d908c323ce946cf1f7d46820f30f0a3cf2c7f46
zhenyaracheva <zhenia.racheva@gmail.com>
2015-11-10 16:43:01 +0200

    Added section service

---
+++ Source/Server/StudentsLearning.Server.Api/Controllers/SectionsController.cs
@@ -0,0 +1,81 @@
+﻿namespace StudentsLearning.Server.Api.Controllers
+{
+    using System.Linq;
+    using System.Web.Http;
+    using System.Web.Http.Cors;
+
+    using Data.Models;
+    using Models;
+    using Services.Data.Contracts;
+
+    [RoutePrefix("api/Sections")]
+    [EnableCors("*", "*", "*")]
+    public class SectionsController : ApiController
+    {
+        private readonly ISectionService sections;
+
+        public SectionsController(ISectionService sectionService)
+        {
+            this.sections = sectionService;
+        }
+
+        public IHttpActionResult Get()
+        {
+            return this.Ok(this.sections.All().ToList());
+        }
+
+        public IHttpActionResult Get(int id)
+        {
+            return this.Ok(this.sections.GetById(id));
+        }
+
+        // TODO: [note] The update of the sections list will be done in post / delete in SectionsController through the foreign key automaticly
+        public IHttpActionResult Put(int id, [FromBody]SectionRequestModel updates)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                return this.BadRequest();
+            }
+
+            var updatedSection = this.sections.GetById(id);
+
+            if (this.sections.GetId(updates.Name) != 0)
+            {
+                return this.BadRequest();
+            }
+            
+            // var newSection = new Section
+            // {
+            //     Name = updates.Name,
+            //     Description = updates.Description,
+            //     CategoryId = updates.CategoryId
+            // };
+
+            updatedSection.Name = updates.Name;
+            updatedSection.Description = updates.Description;
+
+            this.sections.Update(updatedSection);
+
+            return this.Ok();
+        }
+
+        [HttpPost]
+        public IHttpActionResult Post([FromBody] SectionRequestModel sectionModel)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                return this.BadRequest();
+            }
+
+            var section = new Section
+            {
+                Name = sectionModel.Name,
+                Description = sectionModel.Description,
+                CategoryId = sectionModel.CategoryId
+            };
+
+            this.sections.Add(section);
+            return this.Ok(this.sections.GetId(section.Name));
+        }
+    }
+}
---
+++ Source/Services/StudentsLearning.Services.Data/SectionService.cs
@@ -0,0 +1,57 @@
+﻿namespace StudentsLearning.Services.Data
+{
+    using System;
+    using System.Linq;
+    using StudentsLearning.Data.Models;
+    using StudentsLearning.Services.Data.Contracts;
+    using StudentsLearning.Data.Repositories;
+
+    public class SectionService : ISectionService
+    {
+
+        private IRepository<Section> sections;
+
+        public SectionService(IRepository<Section> sections)
+        {
+            this.sections = sections;
+        }
+
+        public void Add(Section section)
+        {
+            this.sections.Add(section);
+            this.sections.SaveChanges();
+        }
+
+        public IQueryable<Section> All()
+        {
+            return this.sections.All();
+        }
+
+        public Section GetById(int id)
+        {
+            return this.All()
+                        .Where(x => x.Id == id)
+                        .FirstOrDefault();
+        }
+
+        public IQueryable<Section> GetByName(string name)
+        {
+            return this.All()
+                       .Where(x => x.Name == name);
+        }
+
+        public int? GetId(string name)
+        {
+            return this.All()
+                       .Where(x => x.Name == name)
+                       .Select(c => c.Id)
+                       .FirstOrDefault();
+        }
+
+        public void Update(Section section)
+        {
+            this.sections.Update(section);
+            this.sections.SaveChanges();
+        }
+    }
+}
