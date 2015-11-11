9a89a2f8b27bac4cc660ae35a9ee41ef8bca2da1
damy90 <dany90@gmail.com>
2015-11-11 19:07:55 +0200

    Adding exception handeling in GoogleDriveHelper

---
+++ Source/GoogleDrive.Api/IDriveService.cs
@@ -0,0 +1,10 @@
+﻿using Google.Apis.Drive.v2;
+using Google.Apis.Services;
+
+namespace GoogleDrive.Api
+{
+    public interface IDriveService : IClientService
+    {
+        FilesResource Files { get; }
+    }
+}
