4501ce36e4ed7a699b5ff2e8f2812c6ddb516aef
damy90 <dany90@gmail.com>
2015-11-14 23:25:55 +0200

    Added example route test, Bugfix: initializing all tests in the project doesn't work for many namespaces if initializing class is in a namespace.

---
+++ Source/StudentsLearning.Services.Api.Tests/RouteTests/CategoriesControllerTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using MyTested.WebApi;
+using NUnit.Framework;
+using StudentsLearning.Server.Api.Controllers;
+using StudentsLearning.Server.Api;
+
+namespace StudentsLearning.Services.Api.Tests.RouteTests
+{
+    [TestFixture]
+    public class CategoriesControllerTests
+    {
+        [Test]
+        public void CategoriesControllerShouldMapCorrectly()
+        {
+            MyWebApi
+                .Routes()
+                .ShouldMap("api/Categories")
+                .To<CategoriesController>(c => c.Get());
+        }
+    }
+}
