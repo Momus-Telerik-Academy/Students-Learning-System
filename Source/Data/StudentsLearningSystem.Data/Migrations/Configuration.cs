003a07fc3b882e11abcad31622835a4972461c0e
MariyaSteffanova <mariya.steffanova@gmail.com>
2015-11-14 20:44:35 +0200

    String connection changed

--- Source/Data/StudentsLearningSystem.Data/Migrations/Configuration.cs
+++ Source/Data/StudentsLearningSystem.Data/Migrations/Configuration.cs
@@ -50,11 +50,7 @@ namespace StudentsLearning.Data.Migrations
                         new Topic() {
                             Title = "Graphs",
                             Content ="Graph algorithms article. Graphs are this and that",
-                            Comments = new HashSet<Comment>
-                            {
-                                new Comment() {UserId=cukiUser.Id, Content="I have learned that when I was 2 years old.. ", Likes=2, Dislikes = 0 },
-                                new Comment() {UserId=peshoUser.Id, Content="Sad panda", Likes=3, Dislikes = 0 }
-                            },
+
                             Examples = new HashSet<Example>
                             {
                                 new Example() { Description = "Graph example", Content="some content"},
