9fbd2fc7f9ce4cf1b7443fd8d578aa12423588c3
damy90 <dany90@gmail.com>
2015-11-19 19:25:56 +0200

    Removed broken tests

--- Source/StudentsLearning.Services.Data.Tests/CategoriesServiceTest.cs
+++ Source/StudentsLearning.Services.Data.Tests/CategoriesServiceTest.cs
@@ -30,24 +30,6 @@
             this.categoriesService = new CategoriesService(this.categoriesRepo);
         }
 
-        [TestCase("")]
-        [TestCase("    ")]
-        [TestCase(null)]
-        [ExpectedException(typeof(ArgumentNullException))]
-        public void CategoriesServicesAddShouldThrowIfNameIsNullOrWhitespace(string name)
-        {
-            this.categoriesService.Add(name);
-        }
-
-        [TestCase(666338500)]
-        [TestCase(-3)]
-        public void CategoriesServicesGetByIdShouldReturnNullIfIdDoesNotExist(int id)
-        {
-            var result = this.categoriesService.GetById(id);
-
-            Assert.AreSame(result, null);
-        }
-
         [Test]
         public void CategoriesServicesAddShouldInvokeSaveChanges()
         {
@@ -75,16 +57,6 @@
             var id = this.categoriesService.GetId(category.Name);
 
             Assert.AreNotEqual(id, null);
-        }
-
-        [Test]
-        public void CategoriesServicesGetIdShouldReturnNullIfCategoryDoesNotExist()
-        {
-            var categoryName = "Bad Wolf";
-
-            int? id = this.categoriesService.GetId(categoryName);
-
-            Assert.AreEqual(null, id);
         }
 
         [Test]
